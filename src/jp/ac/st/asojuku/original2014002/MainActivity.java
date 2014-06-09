2c5eb2a17e89c9cd609c0d1ff34005cbf0d7fbab
aso1201783 <1201783@st.asojuku.ac.jp>
2014-06-09 17:08:41 +0900

    2014/06/09　前田

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,15 +1,57 @@
 package jp.ac.st.asojuku.original2014002;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		Button btnMENTE = (Button)findViewById(R.id.btnMENTE);
+		Button btnHITOKOTO = (Button)findViewById(R.id.btnHITOKOTO);
+
+		btnOK.setOnClickListener(this);
+		btnMENTE.setOnClickListener(this);
+		btnHITOKOTO.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		EditText etv = (EditText)findViewById(R.id.edtHITOKOTO);
+		String inputMsg = etv.getText().toString();
+
+		Intent intent = null;
+
+		switch(v.getId()){
+		case R.id.btnOK:
+			intent = new Intent(MainActivity.this, MainActivity.class);
+			break;
+		case R.id.btnMENTE:
+			intent = new Intent(MainActivity.this, MaintenanceActivity.class);
+			break;
+		case R.id.btnHITOKOTO:
+			intent = new Intent(MainActivity.this, HitokotoActivity.class);
+			break;
+		}
+		startActivity(intent);
+
 	}
 
 	@Override
--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -1,15 +1,48 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MaintenanceActivity extends Activity {
+public class MaintenanceActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maintenance);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Button btnMODORU = (Button)findViewById(R.id.btnMODORU);
+		Button btnDELETE = (Button)findViewById(R.id.btnDELETE);
+
+		btnMODORU.setOnClickListener(this);
+		btnDELETE.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Intent intent = null;
+
+		switch(v.getId()){
+		case R.id.btnMODORU:
+			intent = new Intent(MaintenanceActivity.this, MainActivity.class);
+			break;
+		case R.id.btnDELETE:
+			intent = new Intent(MaintenanceActivity.this, MaintenanceActivity.class);
+			break;
+		}
+		startActivity(intent);
+
 	}
 
 }
